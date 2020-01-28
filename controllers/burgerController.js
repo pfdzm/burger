1682abb4866e7adac2dcfe523b45647c681de026
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-01-28 16:45:32 +0100

    define 3 routes to handle creating, eating and displaying burgers

---
+++ controllers/burgerController.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const router = express.Router();
+
+const burger = require("../models/burgerModel");
+
+router.get("/", (req, res) => {
+  burger.all(data => {
+    const hbsObj = {
+      burgers: data
+    };
+    res.render("home", hbsObj);
+  });
+});
+
+router.post("/api/burgers", (req, res) => {
+  burger.create(req.body.burger_name, data => {
+    res.redirect("/");
+  });
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+  burger.update(req.params.id, data => {
+    if (data.affectedRows == 0) {
+      res.sendStatus(404).end();
+    } else {
+      res.redirect("/");
+    }
+  });
+});
+
+module.exports = router;
