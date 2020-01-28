c845d5b7e39c50a4909ff56a5cb855518d1d28bd
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-01-28 17:33:55 +0100

    deploy to heroku

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,19 @@ const mysql = require("mysql");
 
 require("dotenv").config();
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.DBPW || "passw0rd",
-  database: "burgers_db"
-});
+let connection;
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.DBPW,
+    database: "burgers_db"
+  });
+}
 connection.connect(err => {
   if (err) throw err;
   console.log("connected as id ", connection.threadId);
--- db/schema.sql
+++ db/schema.sql
@@ -5,5 +5,7 @@ USE burgers_db;
 CREATE TABLE IF NOT EXISTS burgers (
     id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(255),
-    devoured BOOLEAN DEFAULT false
+    devoured BOOLEAN DEFAULT false,
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY(id)
 )
