a62c4c445ceea28d320f2b4d4fcb0e3b0077b282
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-01-28 16:44:32 +0100

    set handlebars as view engine, serve static from public folder

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
-const express = require('express')
+const express = require("express");
+const exphbs = require("express-handlebars");
+
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/burgerController");
+
+app.use(routes);
+
+app.listen(PORT, () => console.log("App listening on port ", PORT));
