d488f57af330c35570d37f1f3fcb430e2fe0936a
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-01-28 16:45:57 +0100

    define schema and input some samples

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE IF NOT EXISTS burgers (
+    id INT AUTO_INCREMENT NOT NULL,
+    burger_name VARCHAR(255),
+    devoured BOOLEAN DEFAULT false
+)
---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT into burgers_db.burgers (burger_name)
+VALUES (
+"burger"
+);
+
+INSERT into burgers_db.burgers (burger_name)
+VALUES (
+"cheeseburger"
+);
+
+INSERT into burgers_db.burgers (burger_name)
+VALUES (
+"bbq burger"
+);
